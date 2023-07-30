36e0e005a671b6e4a86866872d7e57685fb3371e
Rohan_iwala <iwalarohan123@gmail.com>
2023-07-30 18:54:49 +0530

    change4

--- Q4/mainchat.js
+++ Q4/mainchat.js
@@ -1,27 +1,30 @@
 
-const websocket = require('ws');
+
 const http = require('http');
-const url = require('url');
-
 const st = require('node-static');
+const chatBot = require('../Q3/chatbot'); // Import chatbot.js module
+const WebSocket = require('ws');
 
-const fileserver = new st.Server('./Q4.html');
+const file = new st.Server('./Q4.html');
 
-const httpserver = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     req.on('end', () => {
-        var get = url.parse(req.url, true).query;
-        fileserver.serve(req, res);
+        file.serve(req, res);
     }).resume();
-}).listen(8080, () => {
-    console.log("http://localhost:8080");
-})
+});
 
-const wss = new websocket.Server({ server: httpserver });
+server.listen(4589, () => {
+    console.log("Server listening on 4589");
+});
+
+const wss = new WebSocket.Server({ server: server });
 
 wss.on('connection', (ws) => {
-    ws.send("hello client..!!");
+    ws.send("Hii, I am a chat bot!!");
 
-    ws.on('message', messgae => {
-        ws.send('I recieved : ' + messgae)
-    })
-})
+    ws.on('message', (data) => {
+        const message = data.toString();
+        const reply = chatBot.chatreplay(message);
+        ws.send(reply);
+    });
+});
